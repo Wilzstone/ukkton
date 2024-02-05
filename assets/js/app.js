14aa1a633bdd92a49db2ab3c18c6a9508eb62cd8
MasBrewaokSoBakso <vwxyzzabcd@gmail.com>
2024-02-05 23:23:16 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#infoText', {
+    strings: ["Jaga diri Anda dan Keluarga saat Perjalanan ", "Disiplin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy", "Ohayo Konichiwa Oyasumi"],
+    typeSpeed: 50,
+    backDelay: 1000,
+    // startDelay: 500,
+    backSpeed: 50,
+    loop: true,
+});

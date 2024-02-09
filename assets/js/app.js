e84e6f59a3a059a94588ce83efafdb358513b5c9
Wilston Laurentius Dai Keraf <wilstonkeraf301@gmail.com>
2024-02-09 19:35:08 +0700

    e

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#infoText', {
-    strings: ["Jaga diri Anda dan Keluarga saat Perjalanan ", "Disiplin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy", "Ohayo Konichiwa Oyasumi"],
+    strings: ["Jaga diri Anda dan Keluarga saat Perjalanan ", "Disiplin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy", "Semoga Selamat Sampai Tujuan :)"],
     typeSpeed: 50,
     backDelay: 1000,
     // startDelay: 500,
---
+++ function/config.php
@@ -0,0 +1,13 @@
+<?php
+// Tambahkan koneksi ke database MySQL
+$servername = "localhost"; // Ganti dengan nama server database Anda
+$username = "root"; // Ganti dengan username database Anda
+$password = ""; // Ganti dengan password database Anda
+$dbname = "pedulidiri"; // Ganti dengan nama database Anda
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
